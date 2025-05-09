dee37a6e4cfe793273069d91455a9536aad2e6f1
Mohit Kumar <133571577+mohitaryan0@users.noreply.github.com>
2025-05-09 20:17:34 +0530

    Fix blog image sizing and update various sections

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
 />
                     </div>
                     <Link
-                      href="https://nextjstemplates.com/templates/saas-starter-startup"
+                      href="/"
                       className="inline-block rounded-xs bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                       >
                       That&rsquo;s me!
@@ -53,7 +53,7 @@ const Hero = () => {
 />
                     </div>
                     <Link
-                      href="https://github.com/NextJSTemplates/startup-nextjs"
+                      href="/"
                       className="inline-block rounded-xs bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                     >
                       That&rsquo;s me!
